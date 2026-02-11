4757b2bdb9cf69bdc790565a2d3ce287b839768a
Nilesh-Mishra267 <nileshkumar26736@gmail.com>
2026-02-12 00:35:24 +0530

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,34 @@
+"use Strict" // treat all JS code as newer version 
+
+//alert(3+3)  // we are using nodejs , not browser
+
+console.log(3
+    +3);   // code readability should high
+    console.log("Hitesh");
+     
+// ECMA => standards //MDN for learning
+
+
+let name="Nilesh";
+let age=25;
+let isLoggedIn =false;
+
+
+//number => 2 to power 53
+//bigInt
+//String=>""
+//boolean =>true/false
+// null=>standalone value
+//undefined=>valuse not assign
+//symbol=>unique
+
+
+//ojbect
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  //object
+
+
+
+
+
