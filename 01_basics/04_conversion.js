acd4df179f9026e5ca0205f4a79fcd43a45492d5
Nilesh-Mishra267 <nileshkumar26736@gmail.com>
2026-02-12 02:11:36 +0530

    conversions

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("2" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >=0);
+
+
+// The reason is that an equality check == and comparisions ><>=<= works differentaly.
+//Comparisions convert null to a number , trating it as 0.
+//That's why (3) null >=0 is true and (1) null > 0 is false.
+
+console.log(undefined === 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict conversion
+
+console.log("2" === 2);
