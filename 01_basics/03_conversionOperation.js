27f0184b83e50f0013551ec94069d8040f24852b
Nilesh-Mishra267 <nileshkumar26736@gmail.com>
2026-02-12 01:37:40 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,6 +33,51 @@ let booleanIsLoggedIn =Boolean(isLoggedIn);
 let  someNumber = 33
 
 let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+// *********************Operations******************************
+
+
+let value = 3;
+let negValue=-value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="Hello";
+let str2 = " Nilesh";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log((3 + 4) * 5 % 7);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter++);
+
+//link to study 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
 
