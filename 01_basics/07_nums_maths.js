6ffeb7a27762bb173dc0ef056d2c0500f7c5f6bd
Nilesh-Mishra267 <nileshkumar26736@gmail.com>
2026-02-13 01:26:10 +0530

    nums-maths

---
+++ 01_basics/07_nums_maths.js
@@ -0,0 +1,42 @@
+const score =  400
+console.log(score);
+
+const balance =new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.2564
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//************** Maths******************* */
+
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(1.6));
+// console.log(Math.ceil(1.6));
+// console.log(Math.floor(1.6));
+// console.log(Math.min(1 , 3 ,6 ,4));
+// console.log(Math.max(1 , 3 ,6 ,4));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min);
+
+
+
+
+
+
+
+
+
+
