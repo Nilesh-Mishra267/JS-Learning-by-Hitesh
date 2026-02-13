a38c09f2bd359d75e0a529fb540125a62da123a7
Nilesh-Mishra267 <nileshkumar26736@gmail.com>
2026-02-14 00:51:42 +0530

    Arrays

---
+++ 02_basics.js/01_arrays.js
@@ -0,0 +1,44 @@
+//Array
+
+const myArray = [0 , 1 , 3 , 4 , 5 , 6]
+const myHeros = ["saktiman", "naagraj"]
+
+const myArr2 = new Array(1 , 2 , 3 , 4)
+
+// console.log(myArray[1]);
+
+ // Array methods
+
+//  myArray.push(7)
+//   myArray.push(8)
+//   myArr2.pop(3)
+
+// myArr2.unshift(9)
+// myArr2.shift(9)
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+
+// const newArr =myArray.join()
+
+// console.log(newArr);
+
+//  console.log(typeof newArr);
+ 
+
+// slice , splice 
+
+console.log("A" , myArray);
+
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B" , myArray);
+
+
+const myn2 = myArray.splice(1,3)
+console.log("C" , myArray);
+console.log(myn2);
+
+
+
