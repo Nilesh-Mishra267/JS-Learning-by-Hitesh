ac3393c4c74e35b30fca486283383e09e3fff6ab
Nilesh-Mishra267 <nileshkumar26736@gmail.com>
2026-02-17 01:07:42 +0530

    function part2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -57,5 +57,36 @@ function loginUserMessage(username ="Mishra"){
 return `${username} just logged in`
 }
 // console.log(loginUserMessage("Nilesh"));
-console.log(loginUserMessage("tiwari"));
+// console.log(loginUserMessage("tiwari"));
 
+
+function caluclateCarPrice (val1 , val2, ...num1){
+    return num1
+
+}
+// console.log(caluclateCarPrice(100,200,300,400));
+
+
+const user ={
+    username : "Nilesh",
+    price : 199
+}
+
+function handleObject(anyobject){
+console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user);
+handleObject({
+    username :"sam",
+    price : 399
+})
+
+
+const myNewArray = [200,400,500,600]
+
+function returnSeondArray(getArray){
+    return getArray[1]
+}
+// console.log(returnSeondArray(myNewArray));
+console.log(returnSeondArray([200,400,500,600]));
